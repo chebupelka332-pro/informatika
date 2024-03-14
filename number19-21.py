820df9721f0323d44718bd7d12b2eea874bcd881
chebupelka332-pro <uchebmaksimtok2006@yandex.ru>
2024-03-14 20:12:00 +0700

    Дополнил 19-21

--- number19-21.py
+++ number19-21.py
@@ -1,4 +1,16 @@
-def f(s, m):
+"""def f(s, m): # простой вариант
+    if s > 33: return m % 2 == 0
+    if m == 0: return 0
+    h = [f(s + 1, m - 1), f(s + 2, m - 1), f(s + 3, m - 1), f(s * 2, m - 1)]
+    return any(h) if (m - 1) % 2 == 0 else all(h)
+
+
+print('19)', [s for s in range(1, 34) if f(s, 2)])
+print('20)', [s for s in range(1, 34) if not f(s, 1) and f(s, 3)])
+print('20)', [s for s in range(1, 34) if not f(s, 2) and f(s, 4)])"""
+
+
+"""def f(s, m): # двойное условие
     if 45 <= s <= 112: return m % 2 == 0
     if s > 112: return m % 2 != 0
     if m == 0: return 0
@@ -6,9 +18,47 @@ def f(s, m):
     return any(h) if (m - 1) % 2 == 0 else all(h)
 
 
-"""m = 1  2  3  4
-       П1 В2 П2 В2"""
-
 print('19)', [s for s in range(1, 45) if f(s, 2)])
 print('20)', [s for s in range(1, 45) if not f(s, 1) and f(s, 3)])
-print('21)', [s for s in range(1, 45) if not f(s, 2) and f(s, 4)])
+print('21)', [s for s in range(1, 45) if not f(s, 2) and f(s, 4)])"""
+
+
+"""def f(s1, s2, m): # две кучи
+    if s1 + s2 >= 59: return m % 2 == 0
+    if m == 0: return 0
+    h = [f(s1 + 1, s2, m - 1), f(s1 * 2, s2, m - 1), f(s1, s2 + 1, m - 1), f(s1, s2 * 2, m - 1)]
+    return any(h) if (m - 1) % 2 == 0 else all(h)
+
+
+print('19)', [s for s in range(1, 53) if f(5, s, 1)])
+print('20)', [s for s in range(1, 53) if not f(5, s, 1) and f(5, s, 3)])
+print('21)', [s for s in range(1, 53) if not f(5, s, 2) and f(5, s, 4)])"""
+
+
+"""def f(s1, s2, m):  # неудачная игра all -> any
+    if s1 + s2 >= 77: return m % 2 == 0
+    if m == 0: return 0
+    h = [f(s1 + 1, s2, m - 1), f(s1 * 2, s2, m - 1), f(s1, s2 + 1, m - 1), f(s1, s2 * 2, m - 1)]
+    return any(h) if (m - 1) % 2 == 0 else all(h)
+
+
+print('19)', [s for s in range(1, 70) if f(7, s, 2)])  # all -> any
+print('20)', [s for s in range(1, 70) if not f(7, s, 1) and f(7, s, 3)])
+print('21)', [s for s in range(1, 70) if not f(7, s, 2) and f(7, s, 4)])"""
+
+
+"""def f(s1, s2, m):  # вычитание
+    if s1 + s2 <= 20: return m % 2 == 0
+    if m == 0: return 0
+    h = [f(s1 - 1, s2, m - 1), f(s1 // 2 + s1 % 2, s2, m - 1),
+         f(s1, s2 - 1, m - 1), f(s1, s2 // 2 + s2 % 2, m - 1)
+         ]
+    return any(h) if (m - 1) % 2 == 0 else all(h)
+
+
+print('19)', [s for s in range(11, 100) if f(10, s, 2)])
+print('20)', [s for s in range(11, 100) if not f(10, s, 1) and f(10, s, 3)])
+print('21)', [s for s in range(11, 100) if not f(10, s, 2) and f(10, s, 4)])"""
+
+
+
